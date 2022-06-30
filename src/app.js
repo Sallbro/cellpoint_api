8fada56ca1919debcf21b84fab18024d5f9d53ff
salman <asusalman986@gmail.com>
2022-06-30 22:02:33 +0530

    cell api

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv')
+dotenv.config({ path: './config.env' });
+const db = require("../src/db/conn");
+const port = process.env.PORT || 5000;
+
+const rout = app.use(require('./routers/routs'));
+
+app.listen(port, () => {
+    console.log(`connection succesful at port ${port}`);
+});
+
+
