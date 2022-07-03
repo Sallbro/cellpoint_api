7d44ac0c8ef55610680ec4696e8ad643651a2de0
salman <asusalman986@gmail.com>
2022-07-03 11:06:49 +0530

    prob req

--- src/app.js
+++ src/app.js
@@ -6,7 +6,12 @@ const db = require("../src/db/conn");
 const port = process.env.PORT || 5000;
 
 const rout = app.use(require('./routers/routs'));
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'Content-Type', 'Authorization');
+    next();
+})
 app.listen(port, () => {
     console.log(`connection succesful at port ${port}`);
 });
